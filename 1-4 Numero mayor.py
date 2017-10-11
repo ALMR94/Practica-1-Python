163cd560cbf28722c811340dc0b245f126f9cb90
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-11 18:21:38 +0200

    Add files via upload

---
+++ 1-4 Numero mayor.py
@@ -0,0 +1,8 @@
+A=int(raw_input("Escribe el primer numero: "))
+B=int(raw_input("Escribe el segundo numero: "))
+print "El numero mayor es:"
+if A > B:
+    print "El primero."
+else:
+    print "El segundo."
+    
